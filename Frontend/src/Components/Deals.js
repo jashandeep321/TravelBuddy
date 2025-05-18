0ba23323060d89d961b43d966161f16df9e4fa03
Jashandeep <jashandeep0673.be21@chitkara.edu.in>
2025-05-18 17:52:33 +0530

    deals responsive

--- Frontend/src/Components/Deals.js
+++ Frontend/src/Components/Deals.js
@@ -1,3 +1,102 @@
+// import React from 'react';
+// import './Deals.css';
+
+// import newuser from './images/destination-Img/newuser.jpg';
+// import winter from './images/destination-Img/winter.jpg';
+// import airline from './images/destination-Img/airline.webp';
+// import cab from './images/destination-Img/cab.jpg';
+// import dutyfree from './images/destination-Img/dutyfree.jpg';
+// import hotel from './images/destination-Img/hotel.jpg';
+// import bus from './images/destination-Img/bus.jpg';
+// import refer from './images/destination-Img/refer.webp';
+// import Footer from "./Footer";
+// import dealimage from './images/destination-Img/deel.png'
+
+// const Deals = () => {
+//   const offers = [
+//     {
+//       id: 1,
+//       title: 'New User Offer',
+//       description: 'Register and get Discount on first bookings with us',
+//       imageUrl: newuser,
+//     },
+//     {
+//       id: 2,
+//       title: 'Winter Sale',
+//       description: 'Enjoy this winter season with exclusive discounts on travel bookings',
+//       imageUrl: winter, 
+//     },
+//     {
+//       id: 3,
+//       title: 'International Airlines Deal',
+//       description: 'Fly to your dream with exclusive deals and discounted fares on international flights',
+//       imageUrl: airline, 
+//     },
+//     {
+//       id: 4,
+//       title: 'Free Cab Offer',
+//       description: 'Enjoy free cab bookings with us',
+//       imageUrl: cab, 
+//     },
+//     {
+//     id: 5,
+//       title: 'Duty Free Products',
+//       description: 'Shop for duty free products on selective airports with extra discounts',
+//       imageUrl: dutyfree,
+//     },
+//     {
+//       id: 6,
+//       title: 'Hotel Offer',
+//       description: 'Upto 20% discount on selected hotel booking',
+//       imageUrl: hotel,
+//     },
+//     {
+//       id: 7,
+//       title: 'Bus Deal',
+//       description: 'Bus tickets to different destinations at a discount up to Rs.500',
+//       imageUrl: bus,
+//     },
+//     {
+//       id: 8,
+//       title: 'Invite & Earn',
+//       description: 'Invite your friends & earn upto Rs.2000',
+//       imageUrl: refer,
+//     },
+//   ];
+
+//   return (
+//     <div>
+//     <div className="deals-page">
+//       <h1 className="deals-heading">Special Deals & Offers</h1>
+//       <p>"Travel More for Less!"
+// Get ready for your next trip with our fantastic travel deals.
+// From family vacations to romantic getaways, find the perfect offer that fits your budget.
+// Save on flights, hotels, and activities - your dream destination is just a click away!
+
+// </p>
+
+//       <img src={dealimage} alt="dealimage" className="dealimage" />
+
+//       <div className="offers-container">
+//         {offers.map((offer) => (
+//           <div className="offer-card" key={offer.id}>
+//             <img src={offer.imageUrl} alt={offer.title} className="offer-image" />
+//             <div className="offer-info">
+//               <h3 className="offer-title">{offer.title}</h3>
+//               <p className="offer-description">{offer.description}</p>
+//             </div>
+//           </div>
+//         ))}
+        
+//       </div>
+//     </div>
+//     <Footer/>
+//     </div>
+//   );
+// };
+
+// export default Deals;
+
 import React from 'react';
 import './Deals.css';
 
@@ -10,7 +109,7 @@ import hotel from './images/destination-Img/hotel.jpg';
 import bus from './images/destination-Img/bus.jpg';
 import refer from './images/destination-Img/refer.webp';
 import Footer from "./Footer";
-import dealimage from './images/destination-Img/deel.png'
+import dealimage from './images/destination-Img/deel.png';
 
 const Deals = () => {
   const offers = [
@@ -24,22 +123,22 @@ const Deals = () => {
       id: 2,
       title: 'Winter Sale',
       description: 'Enjoy this winter season with exclusive discounts on travel bookings',
-      imageUrl: winter, 
+      imageUrl: winter,
     },
     {
       id: 3,
       title: 'International Airlines Deal',
       description: 'Fly to your dream with exclusive deals and discounted fares on international flights',
-      imageUrl: airline, 
+      imageUrl: airline,
     },
     {
       id: 4,
       title: 'Free Cab Offer',
       description: 'Enjoy free cab bookings with us',
-      imageUrl: cab, 
+      imageUrl: cab,
     },
     {
-    id: 5,
+      id: 5,
       title: 'Duty Free Products',
       description: 'Shop for duty free products on selective airports with extra discounts',
       imageUrl: dutyfree,
@@ -66,31 +165,32 @@ const Deals = () => {
 
   return (
     <div>
-    <div className="deals-page">
-      <h1 className="deals-heading">Special Deals & Offers</h1>
-      <p>"Travel More for Less!"
-Get ready for your next trip with our fantastic travel deals.
-From family vacations to romantic getaways, find the perfect offer that fits your budget.
-Save on flights, hotels, and activities - your dream destination is just a click away!
+      <div className="container deals-page">
+        <h1 className="deals-heading">Special Deals & Offers</h1>
+        <p className="lead text-muted">
+          "Travel More for Less!"<br />
+          Get ready for your next trip with our fantastic travel deals.<br />
+          From family vacations to romantic getaways, find the perfect offer that fits your budget.<br />
+          Save on flights, hotels, and activities - your dream destination is just a click away!
+        </p>
 
-</p>
+        <img src={dealimage} alt="dealimage" className="img-fluid dealimage my-4" />
 
-      <img src={dealimage} alt="dealimage" className="dealimage" />
-
-      <div className="offers-container">
-        {offers.map((offer) => (
-          <div className="offer-card" key={offer.id}>
-            <img src={offer.imageUrl} alt={offer.title} className="offer-image" />
-            <div className="offer-info">
-              <h3 className="offer-title">{offer.title}</h3>
-              <p className="offer-description">{offer.description}</p>
+        <div className="row">
+          {offers.map((offer) => (
+            <div className="col-12 col-sm-6 col-lg-3 mb-4" key={offer.id}>
+              <div className="card h-100 shadow-sm offer-card">
+                <img src={offer.imageUrl} alt={offer.title} className="card-img-top offer-image" />
+                <div className="card-body offer-info">
+                  <h5 className="card-title offer-title">{offer.title}</h5>
+                  <p className="card-text offer-description">{offer.description}</p>
+                </div>
+              </div>
             </div>
-          </div>
-        ))}
-        
+          ))}
+        </div>
       </div>
-    </div>
-    <Footer/>
+      <Footer />
     </div>
   );
 };
